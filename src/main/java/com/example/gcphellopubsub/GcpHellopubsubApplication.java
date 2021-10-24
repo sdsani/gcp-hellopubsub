f311f9ea701ca7819d5d7d55a06099bcab1656ef
sdsani <sds.78692@gmail.com>
2021-10-23 22:17:28 -0500

    first commit

---
+++ src/main/java/com/example/gcphellopubsub/GcpHellopubsubApplication.java
@@ -0,0 +1,13 @@
+package com.example.gcphellopubsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpHellopubsubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpHellopubsubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gcphellopubsub/GcpHellopubsubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gcphellopubsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpHellopubsubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
